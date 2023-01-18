b0a8c761b37ddf7f72c1ff15da5f316e00e9c24e
nuukk <84548764+nuukk@users.noreply.github.com>
2023-01-18 10:10:57 +0900

    Update gkp_scrapper.R

--- R/gkp_scrapper.R
+++ R/gkp_scrapper.R
@@ -221,7 +221,7 @@ gkp_simple <- function(keyword,start_date,end_date,country,new_name,print=FALSE)
   }
 }
 
-gkp_read <- function(file_list,country,gbm=NULL,product_type=NULL,type) {
+gkp_read <- function(file_list,country,gbm=NA,product_type=NA,type,save_name,export_dir) {
   if(missing(file_list)) file_list <- choose.files(caption='전처리할 GKP RAW DATA를 선택하세요')
   type <- match.arg(type,choices=c('extended','base'))
   ans <- map(file_list, ~ {
@@ -229,43 +229,28 @@ gkp_read <- function(file_list,country,gbm=NULL,product_type=NULL,type) {
     res <- gkp %>% select(Keyword,starts_with('Search'))
     names(res)[str_detect(names(res),'Searches')] <- gsub("Searches..","",names(res)[str_detect(names(res),'Searches')])
     country <- gsub(country,"",basename(.x))
-    if(!is.null(gbm)) gbm <- gsub(gbm,"",basename(.x))
-    if(!is.null(product_type)) product_type <- gsub(product_type,"",basename(.x))
-    
+    if(!is.na(gbm)) gbm <- gsub(gbm,"",basename(.x))
+    if(!is.na(product_type)) product_type <- gsub(product_type,"",basename(.x))
     if(type=='extended') {
-      res <- res %>% data.table %>% melt(id='Keyword') %>% transmute(country,gbm,product_type,
-                                                                     Keyword=Keyword,
-                                                                     Is_extended=case_when(Keyword==res$Keyword[1] ~ 'Base',TRUE ~ 'Extended'),
-                                                                     Main_keyword=res$Keyword[1],
-                                                                     Date=my(variable),value)
-      if(!is.null(gbm) & !is.null(product_type)) {
-        gkp_res <- res %>% group_by(country,gbm,product_type,Main_keyword,Date,Is_extended) %>% summarize(value=sum(value,na.rm=T)) %>%
-          suppressMessages %>% data.table
-        gkp_res <- gkp_res %>% melt(measure='value') %>% dcast(country+gbm+product_type+Main_keyword+Date~Is_extended,)
-      } else if(!is.null(gbm) & is.null(product_type)) {
-        gkp_res <- res %>% group_by(country,gbm,Main_keyword,Date,Is_extended) %>% summarize(value=sum(value,na.rm=T)) %>%
-          suppressMessages %>% data.table
-        gkp_res <- gkp_res %>% melt(measure='value') %>% dcast(country+gbm+Main_keyword+Date~Is_extended,)
-      } else if(is.null(gbm) & !is.null(product_type)) {
-        gkp_res <- res %>% group_by(country,product_type,Main_keyword,Date,Is_extended) %>% summarize(value=sum(value,na.rm=T)) %>%
-          suppressMessages %>% data.table
-        gkp_res <- gkp_res %>% melt(measure='value') %>% dcast(country+product_type+Main_keyword+Date~Is_extended,)
-      } else {
-        gkp_res <- res %>% group_by(country,Main_keyword,Date,Is_extended) %>% summarize(value=sum(value,na.rm=T)) %>%
-          suppressMessages %>% data.table
-        gkp_res <- gkp_res %>% melt(measure='value') %>% dcast(country+Main_keyword+Date~Is_extended,)
-      }
+      gkp_res <- res %>% data.table %>% melt(id='Keyword') %>% transmute(country,gbm,product_type,
+                                                                         Keyword=Keyword,
+                                                                         Is_extended=case_when(Keyword==res$Keyword[1] ~ 'Base',TRUE ~ 'Extended'),
+                                                                         Main_keyword=res$Keyword[1],
+                                                                         Date=my(variable),value) %>% 
+        group_by(country,gbm,product_type,Main_keyword,Date,Is_extended) %>% summarize(value=sum(value,na.rm=T)) %>% suppressMessages %>% data.table %>% melt(measure='value') %>% dcast(country+gbm+product_type+Main_keyword+Date~Is_extended,)
       gkp_res$Base[is.na(gkp_res$Base)] <- 0
       gkp_res$Extended[is.na(gkp_res$Extended)] <- 0
       gkp_res <- gkp_res %>% rowwise %>% mutate(Extended=sum(Base,Extended,na.rm=T))
     } else {
-      gkp_res <- res %>% data.table %>% melt(id='Keyword') %>%
-        transmute(country,gbm,product_type,keyword=Keyword,month=my(variable),searches_GKP=value)
+      gkp_res <- res %>% data.table %>% melt(id='Keyword') %>% transmute(country,gbm,product_type,keyword=Keyword,month=my(variable),searches_GKP=value)
       gkp_res$searches_GKP[is.na(gkp_res$searches_GKP)] <- 0
-      gkp_res
     }
+    if(is.na(gbm)) { gkp_res <- gkp_res %>% select(-gbm) }
+    if(is.na(product_type)) { gkp_res <- gkp_res %>% select(-product_type) }
+    gkp_res
   }, .progress=TRUE) %>% rbindlist
   ans <- ans %>% filter(get(names(ans)[str_detect(tolower(names(ans)),'keyword')])!="")
-  save_csv(ans,filename='GKP-전처리')
+  if(missing(export_dir)) { export_dir <- choose.dir(caption='전처리된 자료를 저장할 폴더를 선택하세요' )}
+  save_csv(ans,filename=save_name,dir=export_dir)
   ans
 }
